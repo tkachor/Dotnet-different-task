bfeddad1b30b5d520f1954144566a17ea05bbefe
TKachor <tkachor@gmail.com>
2021-03-10 09:24:33 +0200

    Add mai project

---
+++ 1_Classes/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace _1_Classes
+{
+    class Title
+    {
+        public string Titles { get; set; }
+
+        public Title(string titles)
+        {
+            Titles = titles;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Title is: {Titles}");
+            Console.ResetColor();
+        }
+    }
+
+    class Author
+    {
+        public string Authors { get; set; }
+
+        public Author(string authors)
+        {
+            Authors = authors;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Authors is: {Authors}");
+            Console.ResetColor();
+        }
+    }
+
+    class Content
+    {
+        public string Contents { get; set; }
+
+        public Content(string contents)
+        {
+            Contents = contents;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Content is: {Contents}");
+            Console.ResetColor();
+        }
+    }
+
+    class Book
+    {
+        public Title Title { get; set; }
+        public Content Content { get; set; }
+        public Author Author { get; set; }
+
+
+        public Book(string title, string content, string author)
+        {
+            Title = new Title(title);
+            Content = new Content(content);
+            Author = new Author(author);
+        }
+
+        public void Show()
+        {
+            Title.Show();
+            Content.Show();
+            Author.Show();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Zagolowok", "Opisanniya", "Awtor knigi");
+
+            book.Title.Show();
+            book.Content.Show();
+            book.Author.Show();
+
+            book.Show();
+
+        }
+
+    }
+}
---
+++ Dotnet-Esential-Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dotnet_Esential_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
